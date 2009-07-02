9d4a347e4d0f1f3fb9ca013a43bafd723e28e54a
Marcin Kulik <marcin.kulik@gmail.com>
2009-07-02 23:06:52 +0200

    first version

---
+++ css2less.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby
+require 'enumerator'
+
+def add_rule(tree, selectors, style)
+  if selectors.empty?
+    (tree[:style] ||= ";") << style
+  else
+    first, rest = selectors.first, selectors[1..-1]
+    node = (tree[first] ||= {})
+    add_rule(node, rest, style)
+  end
+end
+
+def print_css(tree, indent=0)
+  tree.each do |element, children|
+    puts " " * indent + element + " {\n"
+    style = children.delete(:style)
+    if style
+      puts style.split(";").map { |s| s.strip }.reject { |s| s.empty? }.map { |s| " " * (indent+2) + s + ";" }.join("\n")
+    end
+    print_css(children, indent + 2)
+    puts " " * indent + "}\n"
+  end
+end
+
+tree = {}
+css = File.read(ARGV[0])
+
+css.split("\n").map { |l| l.strip }.join.gsub(/\/\*+[^\*]*\*\//, "").split(/[\{\}]/).each_slice(2) do |style|
+  selectors = style[0].split(",").map { |s| s.strip }.each do |selector|
+    add_rule(tree, selector.split(/\s+/), style[1])
+  end
+end
+
+print_css(tree)
